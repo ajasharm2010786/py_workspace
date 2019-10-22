cd9787e38c48b119b23ab604d0653199b464c9b7
ajasharm2010786 <ajaysharma2010786@gmail.com>
2019-10-23 00:24:39 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,21 @@
 import csv
 import pandas as pd
 import json
+import sys
 
 # https://medium.com/@yzhong.cs/serialize-and-deserialize-complex-json-in-python-205ecc636caa
+
+class Payload(object):
+    def __init__(self, j):
+        self.__dict__ = json.loads(json.dumps(j))
+
+with open('F:\\test2.json', 'r') as f:
+    data = json.load(f)
+    for d in data:
+        p = Payload(d)
+        for k, v in p.answers.items():
+            if isinstance(v, dict):
+                print(v)
 
 
 
